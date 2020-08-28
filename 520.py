2e47484568c4ca4dff9ac60a6c2e2ca556d766b3
noemiems <59896235+noemiems@users.noreply.github.com>
2020-08-27 20:09:11 -0500

    Update 520.py

--- 520.py
+++ 520.py
@@ -1,10 +1,9 @@
 import re
-IN = open("text.txt")
+IN = open("neal.txt")
 OUT = open("output.txt", "w")
 line = "XXX"
 text = []
 sentencelist = []
-exceptions = ['Mr.', 'Mrs.', 'e.g', 'i.e', '.com', 'Dr.', 'Fig.', 'Ms.']
 
 while line:
     line = IN.readline()
@@ -13,12 +12,13 @@ while line:
 
 for x in range(len(text)):
     grouptext = text[x]
-    sentences = re.split('(?<=[.!?]) +(?=[A-Z+])', grouptext)
+    sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)((?<=[.?!])|(?<=\."))(\s|[A-Z].*)', grouptext)
     sentencelist.append(sentences)
 
 for paragraph in range(len(sentencelist)):
     for sentence in sentencelist[paragraph]:
         OUT.write(sentence)
+        OUT.write('\n')
         OUT.write('\n')
         OUT.write('\n')
 
