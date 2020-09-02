241734c86e9d354f376c4379f6366be5a0c984bf
noemiems <59896235+noemiems@users.noreply.github.com>
2020-09-02 08:55:09 -0500

    Update 520.py

--- 520.py
+++ 520.py
@@ -1,37 +1,34 @@
 import re
 
-testString = "\"You are a nice - \" \"So are you!\"\nI love him. I? Love him? Truly! O! 'Tis grand!" #This is a silly test string with some weird examples.
-
-# Can you please fix the code so it can read a .txt file?
-#IN = open("text.txt")
+#testString = "\"You are a nice - \" \"So are you!\"\nI love him. I? Love him? Truly! O! 'Tis grand!" #This is a silly test string with some weird examples.
+IN = open("Test.txt")
 OUT = open("output.txt", "w")
 line = "XXX"
 text = []
 sentencelist = []
 current_sent = ""
 
-# I commented these out but we can add them back later
-#while line:
-#    line = IN.readline()
-#    line.strip()
-#    text.append(line)
+while line:
+    line = IN.readline()
+    line.strip()
+    text.append(str(line))
+    for x in range(len(text)):
+        for token in re.finditer('"*\'*[A-Z]*[a-z]*\!*\.*\?*\-*"*\s*\r*\n*', text[x]):
+            token = token.group(0) #this has something to do with regex groups and parenthesis;
+            current_sent = current_sent + token
+            if re.search('[.!?\-\"](\s|\n|\Z)', token):
+                sentencelist.append(current_sent)
+                current_sent = ""
 
-for token in re.finditer('"*\'*[A-Z]*[a-z]*\!*\.*\?*\-*"*\s*\r*\n*', testString):
-    token = token.group(0) #this has something to do with regex groups and parenthesis;
-    current_sent = current_sent + token
-    if re.search('[.!?\-\"](\s|\n|\Z)', token):
-        sentencelist.append(current_sent)
-        current_sent = ""
+#for x in range(len(sentencelist)):
+#    print(sentencelist[x])
+#    print('\n')
 
-print(sentencelist) #This line is to show that the code is putting what we have decided are sentences into a list.
-
-# Can you please fix the code here so that the output is the content of sentencelist?
-for paragraph in range(len(sentencelist)):
-    for sentence in sentencelist[paragraph]:
-        OUT.write(sentence)
+for x in range(len(sentencelist)):
+        OUT.write(sentencelist[x])
         OUT.write('\n')
         OUT.write('\n')
-        OUT.write('\n')
 
-OUT.close()
+#OUT.close()
 #IN.close()
+
