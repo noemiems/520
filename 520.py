39b31b839de9403a1b7b0cd667fdf77b9def9ff6
kchallis <59855721+kchallis@users.noreply.github.com>
2020-09-02 00:22:21 -0500

    Update 520.py

--- 520.py
+++ 520.py
@@ -1,20 +1,31 @@
 import re
-IN = open("text.txt")
+
+testString = "\"You are a nice - \" \"So are you!\"\nI love him. I? Love him? Truly! O! 'Tis grand!" #This is a silly test string with some weird examples.
+
+# Can you please fix the code so it can read a .txt file?
+#IN = open("text.txt")
 OUT = open("output.txt", "w")
 line = "XXX"
 text = []
 sentencelist = []
+current_sent = ""
 
-while line:
-    line = IN.readline()
-    line.strip()
-    text.append(line)
+# I commented these out but we can add them back later
+#while line:
+#    line = IN.readline()
+#    line.strip()
+#    text.append(line)
 
-for x in range(len(text)):
-    grouptext = text[x]
-    sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<!\sp\.)((?<=[.?!])|(?<=\."))(\s|[A-Z].*)', grouptext)
-    sentencelist.append(sentences)
+for token in re.finditer('"*\'*[A-Z]*[a-z]*\!*\.*\?*\-*"*\s*\r*\n*', testString):
+    token = token.group(0) #this has something to do with regex groups and parenthesis;
+    current_sent = current_sent + token
+    if re.search('[.!?\-\"](\s|\n|\Z)', token):
+        sentencelist.append(current_sent)
+        current_sent = ""
 
+print(sentencelist) #This line is to show that the code is putting what we have decided are sentences into a list.
+
+# Can you please fix the code here so that the output is the content of sentencelist?
 for paragraph in range(len(sentencelist)):
     for sentence in sentencelist[paragraph]:
         OUT.write(sentence)
@@ -23,4 +34,4 @@ for paragraph in range(len(sentencelist)):
         OUT.write('\n')
 
 OUT.close()
-IN.close()
+#IN.close()
