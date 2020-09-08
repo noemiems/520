3015c907b412d9d1313136db8b051b975ec7d57f
noemiems <59896235+noemiems@users.noreply.github.com>
2020-09-08 13:37:51 -0500

    Update 520noemie.py

--- 520noemie.py
+++ 520noemie.py
@@ -1,5 +1,5 @@
 import re
-IN = open("input.txt")
+IN = open("abstracts.txt")
 OUT = open("output.txt", "w")
 line = "XXX"
 text = []
@@ -9,19 +9,22 @@ finallist = []
 while line:
     line = IN.readline()
     line.strip()
+    line = re.sub('<[^>]*>', "\n", line)
     text.append(line)
 
 for x in range(len(text)):
     grouptext = text[x]
-    sentences = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.\s)(?<!p\.)((?<=[.?!])|(?<=\.")|(?<=]\.))(\s+|[A-Z].*)', grouptext)
-    sentencelist.append(sentences)
+    sentences = re.split('(?<=[A-Z]\.[A-Z]\.)(\s)', grouptext)
+    for y in range(len(sentences)):
+        sentencesb = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.\s)(?<!p\.)((?<=[.?!])|(?<=\.")|(?<=]\.))(\s+|[A-Z].*)', sentences[y])
+        sentencelist.append(sentencesb)
 
 for x in range(len(sentencelist)):
     for y in range(len(sentencelist[x])):
         text = sentencelist[x][y]
         final = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.\s)(?<!p\.)((?<=[.?!])|(?<=\.")|(?<=]\.))(\s+|[A-Z].*)', text)
         finallist.append(final)
-        
+
 for paragraph in range(len(finallist)):
     for sentence in finallist[paragraph]:
         OUT.write(sentence)
@@ -29,3 +32,4 @@ for paragraph in range(len(finallist)):
 
 
 OUT.close()
+IN.close()
