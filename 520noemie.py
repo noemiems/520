ef576d36ad11e6ea406778fa6dfb4f400b4bcf73
noemiems <59896235+noemiems@users.noreply.github.com>
2020-09-09 10:54:14 -0500

    Update 520noemie.py

--- 520noemie.py
+++ 520noemie.py
@@ -1,10 +1,15 @@
 import re
-IN = open("abstracts.txt")
+IN = open("input.txt")
 OUT = open("output.txt", "w")
 line = "XXX"
 text = []
 sentencelist = []
 finallist = []
+
+def changingdots( str ):
+    return str.replace('.', '|')
+def changingpipes( str ):
+    return str.replace('|', '.')
 
 while line:
     line = IN.readline()
@@ -14,6 +19,7 @@ while line:
 
 for x in range(len(text)):
     grouptext = text[x]
+    grouptext = re.sub(r"(?:[A-Z]\.)+", lambda m: changingdots(m.group()), grouptext)
     sentences = re.split('(?<=[A-Z]\.[A-Z]\.)(\s)', grouptext)
     for y in range(len(sentences)):
         sentencesb = re.split('(?<!\w\.\w.)(?<![A-Z][a-z]\.\s)(?<!p\.)((?<=[.?!])|(?<=\.")|(?<=]\.))(\s+|[A-Z].*)', sentences[y])
@@ -27,6 +33,7 @@ for x in range(len(sentencelist)):
 
 for paragraph in range(len(finallist)):
     for sentence in finallist[paragraph]:
+        sentence = re.sub(r"(?:[A-Z]\|)+", lambda m: changingpipes(m.group()), sentence)
         OUT.write(sentence)
         OUT.write('\n')
 
