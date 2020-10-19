38110d2e56de2fc5c3d2e72978c569107679661e
noemiems <59896235+noemiems@users.noreply.github.com>
2020-10-19 12:20:00 -0500

    Create pos.py

---
+++ pos.py
@@ -0,0 +1,21 @@
+IN = open("corpus-brown.txt")
+import re
+line = 'XXX'
+text = []
+sentence = []
+wordlist = []
+taglist = []
+tagcount = []
+
+while line:
+    line = IN.readline()
+    line.strip()
+    text.append(line)
+for x in range(len(text)):
+    pattern = re.findall('[A-Z]?[a-z]+/[A-Z]+', text[x])
+    sentence.append(pattern)
+    for y in range(len(sentence)):
+        for z in range(len(sentence[y])):
+            string = sentence[y][z].split('/')
+            wordlist.append(string[0])
+            taglist.append(string[1])
