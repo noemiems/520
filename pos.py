aa33a0a282791d610af1debc1145cb870060418e
noemiems <59896235+noemiems@users.noreply.github.com>
2020-10-20 14:00:53 -0500

    Update pos.py

--- pos.py
+++ pos.py
@@ -1,20 +1,46 @@
-IN = open("corpus-brown.txt")
+IN = open("shortcorpus.txt")
 import re
 line = 'XXX'
 text = []
+ssdict = {}
+dict = {}
+firstword = []
 sentence = []
-wordlist = []
-taglist = []
+tagslist = []
+splitsentence = []
 
 while line:
     line = IN.readline()
     line.strip()
     text.append(line)
 for x in range(len(text)):
-    pattern = re.findall('[A-Z]?[a-z]+/[A-Z]+', text[x])
+    pattern = re.split('\s', text[x])
     sentence.append(pattern)
-    for y in range(len(sentence)):
-        for z in range(len(sentence[y])):
-            string = sentence[y][z].split('/')
-            wordlist.append(string[0])
-            taglist.append(string[1])
+for x in range(len(sentence)):
+    firststring = sentence[x][0].split('/')
+    if len(firststring) == 2:
+        firstword.append(firststring[1])
+for word in firstword:
+    if word not in ssdict:
+        ssdict[word] = 1
+    else:
+        ssdict[word] += 1
+for item in ssdict:
+    probfirst = ssdict[item]/len(firstword)
+    print('Probability that', item, 'starts a sentence:', probfirst)
+for x in range(len(sentence)):
+    for item in sentence[x]:
+        splititem = item.split('/')
+        splitsentence.append(splititem)
+for lists in range(len(splitsentence)):
+    if len(splitsentence[lists]) == 2:
+        tagslist.append(splitsentence[lists][1])
+
+for tag in range(1,len(tagslist)):
+    pair = tagslist[tag-1] + tagslist[tag]
+    if pair not in dict:
+        dict[pair] = 1
+    else:
+        dict[pair] += 1
+
+print(dict)
